b354081cd822d6367cc97c8660ab92fdcfc3a795
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-08 23:30:45 +0300

    calculates points for a word

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,6 +2,15 @@
 import {pointsForWord} from "../utils"
 
 describe("pointsForWord", ()=>{
+ it("handles uppercase and lowercase input", () => {
+    //arrange
+    const word = "tEsT";
+    //act
+    const points = pointsForWord(word);
+    //assert
+    expect(points).toBe(7);
+  })
+   
   it("calculates total points for a word(1 point per vowels, per consonant)", ()=>{
     //arrange
     const word = "test"
