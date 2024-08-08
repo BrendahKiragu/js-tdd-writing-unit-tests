5e6cfcd2a9267d59bb9bebc5cdd16158ed5bfe06
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-08 23:05:18 +0300

    initial commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,13 @@
 // Your tests here
+import {pointsForWord} from "../utils"
+
+describe("pointsForWord", ()=>{
+  it("calculates total points for a word(1 point per vowels, per consonant)", ()=>{
+    //arrange
+    const word = "test"
+    //act
+    const points = pointsForWord(word)
+    //assert
+    expect(points).toBe(7)
+  })
+})
